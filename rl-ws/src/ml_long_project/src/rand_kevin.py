218ceb8847dab8da1673766b0f13d444047a3ec0
Kevin Robb <kevin@js-x.com>
2020-09-27 22:06:32 -0500

    Add states and behavior. Getting stuck in "halt" state. Need to fix.

--- rl-ws/src/ml_long_project/src/rand_kevin.py
+++ rl-ws/src/ml_long_project/src/rand_kevin.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
+#import time
 from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import LaserScan
 
@@ -15,37 +16,85 @@ halt = False
 fwd_scan = None
 rear_scan = None
 l15_scan = None
+l_scan = None
 r15_scan = None
-
+r_scan = None
+# current time
+cur_time = 0
+# current state. can be either "init", "drive, "halt", "turn_r", "turn_l"
+cur_state = "init"
+# current forward and turn speeds
+fwd_spd = None
+turn_spd = None
 
 def check_scan(scan_msg):
-    global halt, fwd_scan, rear_scan, l15_scan, r15_scan
+    global fwd_scan, rear_scan, l15_scan, l_scan, r15_scan, r_scan
     # scan_msg.ranges is an array of 640 elements representing 
     # distance measurements in a full circle around the robot (0=fwd, CCW?)
 
     # update the important entries (TODO verify these indexes)
-    fwd_scan = scan_msg.ranges[0]
-    rear_scan = scan_msg.ranges[320]
-    l15_scan = scan_msg.ranges[30]
-    r15_scan = scan_msg.ranges[610]
-    print([fwd_scan, l15_scan, rear_scan, r15_scan])
+    fwd_scan = scan_msg.ranges[0] # directly forward
+    rear_scan = scan_msg.ranges[320] # directly behind
+    l15_scan = scan_msg.ranges[30] # ~15 degrees left
+    l_scan = scan_msg.ranges[160] # 90 degrees left
+    r15_scan = scan_msg.ranges[610] # ~15 degrees right
+    r_scan = scan_msg.ranges[480] # 90 degrees right
 
-    # if scan_msg.ranges[0] < 1.0:
-    #     halt = True
-
+def check_state():
+    global fwd_spd, turn_spd, cur_state
+    if cur_state == "init":
+        # start by just going straight forwards
+        cur_state = "drive"
+        fwd_spd = 1
+        turn_spd = 0
+    elif cur_state == "drive" and fwd_scan > 0 and fwd_scan < 1:
+        cur_state = "halt"
+        fwd_spd = 0
+        turn_spd = 0
+    elif cur_state == "halt":
+        # check surroundings and decide where to go
+        if r_scan < 0.001 or r_scan > 2:
+            cur_state = "turn_r"
+            fwd_spd = 0.1
+            turn_spd = 1
+        elif l_scan < 0.001 or l_scan > 2:
+            cur_state = "turn_l"
+            fwd_spd = 0.1
+            turn_spd = -1
+        #else #do nothing for now, stay in halt
+    elif cur_state == "turn_r" or cur_state == "turn_l":
+        # turn in place until there is free space ahead
+        # or TODO turn for a set amount of time that causes a 90 degree turn
+        if fwd_scan < 0.001 or fwd_scan > 2:
+            cur_state = "drive"
+            fwd_spd = 1
+            turn_spd = 0
 
 def send_command(timer_event):
+    global fwd_spd, turn_spd
+
+    check_state()
+
+    # tell us the current state for debug
+    print(cur_state)
+    print([fwd_scan, l_scan, rear_scan, r_scan])
+
+    # don't try to do things before fwd_spd and turn_spd have been set
+    if cur_state == "init":
+        return
+
     if halt:
-        fwd_speed = 0
-        turn_speed = 0
-    else:
-        # set random forward and turn speeds (TODO turn away from walls)
-        fwd_speed = 1
-        turn_speed = 0
+        fwd_spd = 0
+        turn_spd = 0
+    # else:
+    #     # set random forward and turn speeds (TODO turn away from walls)
+    #     fwd_spd = 1
+    #     turn_spd = 0
+
     # x-direction is forward
-    lin_vel = Vector3(fwd_speed, 0, 0)
+    lin_vel = Vector3(fwd_spd, 0, 0)
     # turn around z-axis to stay within xy-plane
-    ang_vel = Vector3(0, 0, turn_speed)
+    ang_vel = Vector3(0, 0, turn_spd)
 
     rand_cmd.linear = lin_vel
     rand_cmd.angular = ang_vel
