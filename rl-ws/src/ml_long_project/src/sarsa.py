aa84da7894c386bfd27d7bb2f10133242765fbc7
Logan <lelliott20@ou.edu>
2020-10-10 09:53:51 -0500

    Sarsa now executes, isn't really tested for accuracy

--- rl-ws/src/ml_long_project/src/sarsa.py
+++ rl-ws/src/ml_long_project/src/sarsa.py
@@ -11,6 +11,7 @@ import numpy as np
 from scipy.spatial import distance
 import copy
 import pickle
+from getpass import getuser
 
 ## Global Variables
 # mobile_base velocity publisher
@@ -69,6 +70,10 @@ def check_state():
             train_sarsa(map_name, map,start_point,goal_point)
 
 def train_sarsa(map_name, map,start_point, goal_point):
+    alpha = .5
+    gamma = .9
+    eps  = .1
+
     # Save out data to map_name.sarsa so we don't have to retrain on familiar maps
     #pickle.dump(map, open("sarsa_data/"+map_name + ".sarsa","wb"))
     # Load prior Q or init to zero
@@ -82,29 +87,86 @@ def train_sarsa(map_name, map,start_point, goal_point):
         q[goal_point.x, goal_point.y, 2] = 0
         q[goal_point.x, goal_point.y, 3] = 0
 
-    point = start_point
     episode = 0
     # Episode
     while episode < 5:
         timeout = 0
-        point = start_point
+        path = []
+        # a is action
+        # 0 is North
+        # 1 is East
+        # 2 is South
+        # 3 is West
+        # s is state and equals a point in the map
+        s = start_point
         # Choose A from S using policy dervied from Q (e.g. e-greedy)
-        s = 0
+        a = e_greedy(eps, q, s)
+        path.append(a)
         # Loop through episode
-        while timeout < 5000 and point != goal_point:
+        while timeout < 5000 and s != goal_point:
             # Take action A, observe R,S'
+            r, s_prime = execute(a,s,map)
+            
             # Choose A' from S' using policy dervied from Q (e.g. e-greedy)
+            a_prime = e_greedy(eps, q, s_prime)
+
             # Q(S,A) <- Q(S,A) + alpha[R+gamma * Q(S',A')- Q(S,A)]
+            q[s.x,s.y,a] = q[s.x,s.y,a] + alpha * (r+gamma*q[s_prime.x,s_prime.y,a_prime] - q[s.x,s.y,a])
             # S<- S'; A<-A';
-            s =0
+            s = s_prime
+            a = a_prime
+            path.append(a)
         episode +=1
 
     pickle.dump(q, open(map_name + ".sarsa","wb"))
+    print(path)
+    return path
 
 def sarsa(map,start_point,goal_point):
     return 0
 
+def e_greedy(eps, q, s):
+    p = np.random.random() 
+    if p < eps: 
+      a = np.random.randint(0,4) 
+    else: 
+      a = np.argmax([action for action in q[s.x,s.y]]) 
+    return a
 
+def execute(action, state, map):
+    # IMPORTANT: Execute does not factor in heading. It is only based on finding a coordinate path similar to A* and Potential Fields
+    # Ie North goes up a y val, South decrements etc
+    # Reward is always -1. Unless it tries to go into a wall, it doesn't move and gets a -5
+    if(action == 0): # North
+        if((state.x + 1) < map.shape[0] and map[state.y,state.x+1] != 1):
+            r = -1
+            s_prime = Pose2D(state.x + 1,state.y,0)
+        else: 
+            s_prime = state 
+            r = -5
+    elif(action == 1): # East
+        if((state.y + 1) < map.shape[0] and map[state.y+1, state.x] != 1):
+            r = -1
+            s_prime = Pose2D(state.x,state.y+1,0)
+        else: 
+            s_prime = state 
+            r = -5
+    elif(action == 2): # South
+        if((state.x - 1) > 0 and map[state.y, state.x-1] != 1):
+            r = -1
+            s_prime = Pose2D(state.x -1,state.y,0)
+        else: 
+            s_prime = state 
+            r = -5    
+    else: # West
+        if((state.y - 1) > 0 and map[state.y-1, state.x] != 1):
+            r = -1
+            s_prime = Pose2D(state.x,state.y-1,0)
+        else: 
+            s_prime = state 
+            r = -5
+
+    return r, s_prime
 def get_map(map_msg):
     global map
     #print(map_msg.info)
@@ -119,7 +181,7 @@ def get_map(map_msg):
     small_map = np.zeros((int(map_msg.info.width*resolution)+1,int(map_msg.info.width*resolution)+1))
     
     # Save it out to view at as a csv
-    # np.savetxt("/home/lelliott/turtlepath/rl-ws/demofile2.csv", big_map, delimiter=",")
+    #np.savetxt("/home/lelliott/turtlepath/rl-ws/demofile2.csv", big_map, delimiter=",")
 
     for x in range(small_map.shape[0]):
         for y in range(small_map.shape[1]):
@@ -137,6 +199,7 @@ def get_map(map_msg):
     small_map = np.fliplr(np.flipud(small_map.transpose()))
     print(small_map)
     map = small_map
+    np.savetxt("/home/"+getuser()+"/turtlepath/rl-ws/map.csv", map, delimiter=",")
 
 
 def send_command(keyword):
